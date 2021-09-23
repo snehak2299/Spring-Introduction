8813318a3ac8504796b2b3ca9052fe5274d60621
snehak2299 <skanase2299@gmail.com>
2021-09-23 18:26:46 +0530

    [sneha] created rest controller

---
+++ src/main/java/com/bridglab/demo/controller/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.bridglab.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+	@RequestMapping("/")
+	public String index() {
+		return "Hello from sneha kanase";	
+	}
+	
+}
